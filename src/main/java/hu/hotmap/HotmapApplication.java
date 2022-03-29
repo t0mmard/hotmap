a603da7a2a3b1cfa9308172116b5dae7620191a8
Troszt Tamás <TrosztT@kopdat.local>
2022-03-29 23:57:02 +0200

    Updating main

--- src/main/java/hu/hotmap/HotmapApplication.java
+++ src/main/java/hu/hotmap/HotmapApplication.java
@@ -7,8 +7,20 @@ public class HotmapApplication {
 
     static Arguments args;
 
-    public static void main(String[] argv){
+    public static void main(String[] argv) throws Exception{
         initParams(argv);
+        System.out.println("Starting application:");
+        System.out.println("\tband5: " + args.b5 + ", band6: " + args.b6 + ", band7: " + args.b7 + "\n");
+        var imageLoader = new ImageLoader();
+
+        var bands = imageLoader.loadBands();
+
+        NovelAlgorithm algorithm = new NovelAlgorithm();
+        var values = algorithm.run(bands);
+
+        Printer printer = new Printer();
+        var img = printer.createBufferedImage(values);
+        printer.writeImgToFile(img, args.outputFile);
     }
 
     static void initParams(String[] argv){
@@ -18,4 +30,5 @@ public class HotmapApplication {
                 .build()
                 .parse(argv);
     }
+
 }
